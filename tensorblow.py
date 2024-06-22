a2a134f909078fb5953d7dff57debf5063b3bddf
John Caruthers <john.d.caruthers@gmail.com>
2024-06-22 14:42:25 -0400

    Update tensorblow.py
    the input vector for deep layers over 2 contain in itself an input vector. Need to eliminate the layers

--- tensorblow.py
+++ tensorblow.py
@@ -28,7 +28,7 @@ class Node:
     def calc_value(self):
         'Calculate dot product of weights and inputs'
         temp_sum = 0
-        end = len(self.input_vector.inputs)
+        end = self.input_vector.layer_size
         for i in range(end):
             temp_sum += self.input_vector.inputs[i] * self.weights[i]
         self.value = temp_sum + self.bias
@@ -178,9 +178,10 @@ Y = [1,0]
 test_network = Network(X, Y)
 test_network.dense(3)
 test_network.dense(3)
+
 test_network.printL(1)
-test_network.printL(2)
+# test_network.printL(2)
 test_network.forward_prop()
 print("==================")
 test_network.printL(1)
-test_network.printL(2)
+# test_network.printL(2)

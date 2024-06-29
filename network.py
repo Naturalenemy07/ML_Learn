c83e574d914bf496e4234714e030f7cb5b2462a9
John Caruthers <john.d.caruthers@gmail.com>
2024-06-28 22:41:46 -0400

    Update network.py

--- network.py
+++ network.py
@@ -17,6 +17,14 @@ class Layer:
     def __init__(self, size, activation):
         self.size = size
         self.activation = activation
+        self.bias = []
+        self.weights = [[]]
+    
+    def random_weights(self):
+        temp_weights = []
+        for i in range(self.size):
+            temp_weights.append(random.random())
+        
 
 class Network:
     def __init__(self, input_data, labels):

6ed2f4e24514d9da683ca97b6fc4c2f3d361ee72
John Caruthers <john.d.caruthers@gmail.com>
2024-06-30 22:27:54 -0400

    Update network.py

--- network.py
+++ network.py
@@ -23,17 +23,23 @@ class Layer:
 
     def __random_weights(self):
         list_all_weights = []
-        for j in range(self.num_inputs):
+        for j in range(self.size):
             temp_weights = []
-            for i in range(self.size):
+            for i in range(self.num_inputs):
                 temp_weights.append(random.random())
             list_all_weights.append(temp_weights)
         return list_all_weights
-
-        
 
 class Network:
     def __init__(self, input_data, labels):
         self.input_data = input_data
         self.labels = labels
+
+    def forward(self):
+        pass
     
+test_layer = Layer(3, 2, 'relu')
+test_weights = test_layer.weights
+print(test_weights)
+
+

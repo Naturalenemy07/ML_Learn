c84e3abdada1031c514c29d9a650c8d189d7ebfa
John Caruthers <john.d.caruthers@gmail.com>
2024-07-04 00:48:44 -0400

    Update network.py

--- network.py
+++ network.py
@@ -18,18 +18,24 @@ class Layer:
         self.size = size
         self.num_inputs = num_inputs
         self.activation = activation
-        self.bias = []
+        self.bias = self.__random_bias()
         self.values = []
         self.weights = self.__random_weights()
 
     def __random_weights(self):
-        list_all_weights = []
+        all_temp_weights = []
         for j in range(self.size):
             temp_weights = []
             for i in range(self.num_inputs):
                 temp_weights.append(random.random())
-            list_all_weights.append(temp_weights)
-        return list_all_weights
+            all_temp_weights.append(temp_weights)
+        return all_temp_weights
+    
+    def __random_bias(self):
+        temp_bias = []
+        for i in range(self.size):
+            temp_bias.append(random.randrange(10))
+        return temp_bias
 
 class Network:
     def __init__(self, input_data = [], labels = []):
